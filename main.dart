e3c8e85dd3ea9658f85add15f81b14c9b3433148
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-19 14:24:43 +0000

    notes for mathematic operations.

--- main.dart
+++ main.dart
@@ -1,17 +1,7 @@
 void main() {
-  var x = 10;
-  print('x = $x');
-
-  // var y = x.toDouble();
-  var y = x.toString();
-      
-  print('x = $x');
-  print(y + ' is  the value');
-  print(x.runtimeType);
-  print(y.runtimeType);
-
-  var num1 = '22';
-  var num2 = num.parse(num1);
-
-  print(num2.runtimeType);
+  var x = 9;
+  var y = 4;
+  var z = x != y && 0 < y;
+  var a = x is double;
+  print('z = $a');
 }
