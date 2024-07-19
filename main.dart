6eb5e8d5beb5c9900115bf4126f59bae2e56cdca
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-19 17:53:39 +0000

    Grading Application

--- main.dart
+++ main.dart
@@ -1,18 +1,17 @@
+import 'dart:math';
+
 void main() {
-      var i = 0;
-      while(i < 11) {
-            print("$i - Hi!");
-         i++;   
-      }
-
-      i = 0;
-
-      do {
-            print('$i-Hi');
-            i++;
-      } while(i < 11);
-
-      for(var i = 0; i < 11; i++){
-            print('$i-Hi');
-      }
+  var degree = Random().nextInt(100);
+  print('Degree = $degree');
+  if (degree >= 90) {
+    print("You got an A");
+  } else if (degree >= 80) {
+    print("You got a B");
+  } else if (degree >= 70) {
+    print("You got a C");
+  } else if (degree >= 60) {
+    print("You got a D");
+  } else {
+    print("You got a F");
+  }
 }
