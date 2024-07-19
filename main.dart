359f4d79b990f0d958db61868bd52b650d036e68
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-19 12:41:07 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+  print('Hello World!');
+
+  print("Katie's Lessons");
+
+  print('''
+        Katie
+        Schwerer
+        ''');
+
+  print(3+5);
+  print('3+5 = ${3 + 5}');
+}
