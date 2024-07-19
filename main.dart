738355bcaca05b4160c0eb180bdf9915150ebcba
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-19 19:04:13 +0000

    exceptions

--- main.dart
+++ main.dart
@@ -1,10 +1,18 @@
 import 'dart:io';
 
 void main() {
-      print('Enter your Birth Year: ');
-      var birthYear = num.parse(stdin.readLineSync()!);
-      var age = DateTime.now().year - birthYear;
-      print('Your Age is $age years old');
-
       //Exception
+      do {
+            try {
+                  print('Enter your Birth Year: ');
+                  var birthYear = num.parse(stdin.readLineSync()!);
+                  var age = DateTime.now().year - birthYear;
+                  print('Your Age is $age years old');
+                  break;
+            } on FormatException {
+                  print('Invalid Value');
+            } catch (e) {
+                  print('Error Message: $e');
+            }
+      } while (true);
 }
