f7d2e2bfa36d53292986306dba6a32e873930ffc
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-19 15:12:04 +0000

    notes about conditional operator

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
 void main() {
+      var a = 4;
+      var b = 3;
+      var smallerNum = a < b ? a : b;
+
       
+      //a < b ? print('$a is smaller') : print('$b is smaller');
+      print(smallerNum);
+
+      var name;
+      var v1 = name ?? 'Katie';
+      print(v1);
 }
