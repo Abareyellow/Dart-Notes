4c8ea364c9c5cdc4a9109cf1a405f29b3c6680ff
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-22 15:15:03 +0000

    notes about lambda

--- main.dart
+++ main.dart
@@ -1,17 +1,17 @@
 void main() {
-      info();
+      var list = [3, 5, 7, 9];
+      list.forEach(b);
+      a();
+      lam1();
+      print(lam2());
+      lam3(3, 4);
+      print(lam4(3, 4));
 }
 
-void info() {
-      try {
-            for (var i = 0; i <= 10; i++) {
-                  if (i == 5) {
-                        throw FormatException;
-                  }
+var b = (element) => print(element);
+void lam1() => print(3 + 4);
+int lam2() => 3 + 4;
+void lam3(x, y) => print(x + y);
+int lam4(x, y) => x + y;
 
-                  print('i=$i');
-            }
-      } catch (e) {
-            print(e);
-      }
-}
+void Function() a = () => print(2 + 2);
