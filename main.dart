b34eb461db764b77c7ee9dfbbc484b4ef3f59499
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-22 18:44:48 +0000

    notes about static method

--- main.dart
+++ main.dart
@@ -1,12 +1,13 @@
 void main() {
       //var h1 = Human();
-      Human h1 = Human("Katie", 24, 100.5);
+      Human h1 = Human();
+      Human.values("Katie", 24, 100.5);
 }
 
 class Human {
       var name, age, height;
 
-      Human(String name, int age, double height) {
+      static void values(String name, int age, double height) {
             name = name;
             age = age;
             height = height;
