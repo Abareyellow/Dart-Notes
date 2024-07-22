592b0512d5f725a85c172b018a40cba05965598d
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-22 15:53:30 +0000

    notes about where functions

--- main.dart
+++ main.dart
@@ -1,17 +1,7 @@
 void main() {
-      var list = [3, 5, 7, 9];
-      list.forEach(b);
-      a();
-      lam1();
-      print(lam2());
-      lam3(3, 4);
-      print(lam4(3, 4));
+      var a1 = [1, 2.5, 3.2, 4, 5, 6];
+      print('a1 = $a1');
+
+      var b1 = a1.where((element) => element < 5).toList();
+      print('b1 = $b1');
 }
-
-var b = (element) => print(element);
-void lam1() => print(3 + 4);
-int lam2() => 3 + 4;
-void lam3(x, y) => print(x + y);
-int lam4(x, y) => x + y;
-
-void Function() a = () => print(2 + 2);
