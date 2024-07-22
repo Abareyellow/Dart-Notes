9c0c26b5577aa327815e75d36e65125c1863dd94
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-22 12:49:51 +0000

    notes about 2D Array

--- main.dart
+++ main.dart
@@ -1,10 +1,23 @@
 import 'dart:io';
 
 void main() {
-      List<int> li = [2, 4, 6, 8];
-      List<String> li2 = ['a', 'b'];
-      List<double> li3 = [3.2, 2.2];
-      List<dynamic> li4 = [2, 4.3, 'a', true];
-      print(li);
-      //Done
+      var a = [2, 4, 6];
+
+      for (var i = 0; i < a.length; i++) {
+          print(a[i]);  
+      }
+
+      var b = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+      for (var i = 0; i < b.length; i++) {
+            for (var j = 0; j < b[i].length; j++) {
+                  print(b[i][j]);
+            }
+      }
+
+      for (var item in b) {
+            for (var i in item) {
+                  print(i);
+            }
+      }
 }
