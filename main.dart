daf33cb7a42d8c52229b721e240c61716c93ec96
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-23 15:51:48 +0000

    notes  about Lexical Closure

--- main.dart
+++ main.dart
@@ -1,43 +1,13 @@
 void main() {
-      //var s = Shape();
-      var c = Circle();
-      var r = Rectangle();
-      var sq = Square();
+      Function fun() {
+            var message;
+            Function say = (String msg) {
+                  message = msg;
+                  print(message);
+            };
+            return say;
+      };
 
-      var a = A();
-      a.fun(c);
-      //s.info();
-      c.info();
-      r.info();
-      sq.info();
-
-      List<Shape> data = [c, r, sq];
-      for (var item in data) {
-            item.info();
-      }
-}
-
-class A {
-      void fun(Shape q) {
-            q.info();
-      }
-}
-
-abstract class Shape {
-      void info() => print('Shape');
-}
-
-class Circle extends Shape {
-      @override
-      void info() => print('Circle');
-}
-
-class Rectangle extends Shape {
-      @override
-      void info() => print('Rectangle');
-}
-
-class Square extends Shape {
-      @override
-      void info() => print('Square');
+      var f = fun();
+      f('Hi!');
 }
