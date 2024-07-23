ef4a5094480505c417a9233bdd38cf2e49fd54c2
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-23 18:45:04 +0000

    oj;pj

--- main.dart
+++ main.dart
@@ -1,11 +1,5 @@
+import 'package:http/http.dart' as http;
+
 void main() {
-      var human1 = Human();
-
-      human1('Katie', 24);
-}
-
-class Human {
-      void call(String name, int age) {
-            print('Name = $name || Age = $age');
-      }
+      
 }
