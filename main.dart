208fcaf969c19200695b63107934412cef90d2f9
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-23 14:51:52 +0000

    notes about Generic

--- main.dart
+++ main.dart
@@ -1,24 +1,18 @@
 void main() {
-      print(info(Color.red));
-      print(Color.values[1]);
-      //print(Color2.red);
+      var a = A<int, double>();
+      a.x = 10;
+      a.y = 3.4;
+      print(a.x);
+      print(a.y);
+
+      var b = A<String, bool>();
+      b.x = "Katie";
+      b.y = true;
+      print(b.x);
+      print(b.y);
 }
 
-enum Color { red, blue, black }
-
-/*class Color2 {
-      static const red = "red";
-      static const blue = "blue";
-      static const black = "black";
-}*/
-
-String info(Color s) {
-      switch(s) {
-            case Color.red:
-                  return 'red';
-            case Color.blue:
-                  return 'blue';
-            case Color.black:
-                  return 'black';
-      }
+class A <T, E> {
+      late T x;
+      late E y;
 }
