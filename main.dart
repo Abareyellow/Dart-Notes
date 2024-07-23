2cf5bff2d0aaa2eeedfe3b6392d2c8b81b0f821f
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-23 12:27:28 +0000

    notes about mixins

--- main.dart
+++ main.dart
@@ -1,22 +1,27 @@
-abstract class A {
-      void info();
+mixin class A {
+      void info() {
+            print("Hello");
+      }
 }
 
-abstract class B extends A {
-      void info2();
+mixin class B {
+      void info2() {
+            print("Hello - 2");
+      }
 }
       
-
-class C implements B, A {
-      @override
-      void info() => print("Hi");
-
-      @override
-      void info2() => print("Hello");
+abstract class C {
+      void info3();
 }
 
+class D extends C with A, B {
+      @override
+      void info3() => print("Hello - 3");
+} 
+
 void main() {
-      var c = C();
-      c.info();
-      c.info2();
+      var d = D();
+      d.info();
+      d.info2();
+      d.info3();
 }
