ff29e8c098986c90ef36d1eea837828309983a03
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-25 15:14:48 +0000

    Done

--- main.dart
+++ main.dart
@@ -1,5 +1,17 @@
-import 'package:http/http.dart' as http;
+class MathQuestion {
+      String question;
+      double answer;
+
+      MathQuestion({required this.question, required this.answer});
+}
+
+double getAnswer(String msg) {
+      print('Q// $msg');
+      var answer = double.parse(stdin.reaLineSync());
+}
 
 void main() {
-      
+     MathQuestion(question: '4 + 5', answer: 9.0); 
+      MathQuestion(question: '8 * 100', answer: 800.0); 
+      MathQuestion(question: '8 - 4', answer: 4.0); 
 }
