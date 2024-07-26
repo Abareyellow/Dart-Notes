c0c89a5b888dd7a9d00f4afe31072ae24b8a8f3f
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-07-26 12:11:33 +0000

    Notes about Dart Maps from Dart & Flutter

--- main.dart
+++ main.dart
@@ -1,28 +1,9 @@
 void main() {
-     Vehicle car1 = new Vehicle("V6");
-      Vehicle car2 = new Vehicle("V12");
-      SuperCar car3 = new SuperCar("V16");
-      car1.display();
-      car2.display();
-      car3.display();
-}
-
-class Vehicle {
-      String engine;
-
-      /*Car(String eng) {
-            this.engine = eng;
-      }*/
-
-      Vehicle(this.engine) {
-            
-      }
-
-      void display() {
-            print(engine);
-      }
-}
-
-class SuperCar extends Vehicle {
-      SuperCar(String engine) : super(engine);
+     var cars = {"Tesla": "Electric", "Toyota" : "Gasoline"};
+      print(cars);
+      var fruits = new Map();
+      fruits["Apple"] = "Red";
+      print(fruits);
+      var userAges = {"Jeff" : 22, "George" : 31};
+      print(userAges["George"]);
 }
